e2120680d80e4fee17966b886814f3d0dfb36af2
Robsongdev <robsong369@gmail.com>
2021-07-08 19:06:56 -0300

    :construction: Create BoardGame object without CSS

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
 
-import CardGame from './src/components/CardGame'
+import BoardGame from './src/objects/BoardGame'
 
 const $root = document.querySelector('#root')
-const $htmlCardGame = CardGame() //return da função executada
+const $htmlBoardGame = BoardGame(6);
 
 
-$root.insertAdjacentHTML('beforeend', $htmlCardGame)
+$root.insertAdjacentHTML('beforeend', $htmlBoardGame)
 
