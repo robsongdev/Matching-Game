462fd842f5dc6f0b372a6e5e9fcd5a7d380a8776
Robsongdev <robsong369@gmail.com>
2021-07-09 19:46:04 -0300

    :lipstick: Create ScoreBoard object

--- main.js
+++ main.js
@@ -3,17 +3,16 @@ import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
 
 import BoardGame from './src/objects/BoardGame'
-import PlayerName from './src/components/PlayerName'
+import ScoreBoard from './src/objects/ScoreBoard'
+
 
 const $root = document.querySelector('#root')
-const $htmlBoardGame = BoardGame(6);
-const $htmlPlayerName = PlayerName()
+
 
 
 $root.insertAdjacentHTML('beforeend', 
     `  
-    ${PlayerName('Player 1')}
-    ${PlayerName('Player 2')}
+    ${ScoreBoard()}
     ${BoardGame(6)}
 
     `)
